521434c5eb197a069dbe256adc5d1753d11fd898
Ali Kavoosi <54308350+ali77sina@users.noreply.github.com>
2021-12-25 01:43:56 +0100

    Create example.py

---
+++ example.py
@@ -0,0 +1,9 @@
+import numpy as np
+inds = [[100,120],[200,250]]
+data = np.zeros(shape = (5,500))
+labels = np.zeros(500)
+for i in inds:
+  data[:,i[0]:i[1]] = 1
+  labels[i[0]:i[1]] = 1
+segs = segment(data,labels)
+epochs = segs.epochs()
